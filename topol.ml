181720162144b8c5e8b12e78074658f303466219
Marcin <92443163+kapustaikwas27@users.noreply.github.com>
2021-12-20 12:16:01 +0100

    Wersja finalna

--- topol.ml
+++ topol.ml
@@ -7,32 +7,44 @@
 exception Cykliczne
 
 let topol lista = 
-    let tworz_graf lista = (* utworzenie grafu w postaci mapy list *)
+    (** utworzenie grafu w postaci mapy list *)
+    let tworz_graf lista = 
         let pom a h =
-            let b = List.fold_left (fun a h -> if PMap.mem h a then a else PMap.add h [] a) a (snd h) in (* mapa po przetworzeniu sąsiadów wierzchołka *)
-            if PMap.mem (fst h) b then PMap.add (fst h) ((snd h)@(PMap.find (fst h) b)) b  (* dołączenie sąsiadów wierzchołka do mapy, w której już jest wierzchołek *) 
-            else PMap.add (fst h) (snd h) b  (* dołączenie sąsiadów wierzchołka do mapy, w której jeszcze nie ma wierzchołka *) 
+            (** mapa po przetworzeniu sąsiadów wierzchołka *)
+            let b = List.fold_left (fun a h -> if PMap.mem h a then a else PMap.add h [] a) a (snd h) in 
+            (** dołączenie sąsiadów wierzchołka do mapy, w której już jest wierzchołek *) 
+            if PMap.mem (fst h) b then PMap.add (fst h) ((snd h) @ (PMap.find (fst h) b)) b  
+            (** dołączenie sąsiadów wierzchołka do mapy, w której jeszcze nie ma wierzchołka *) 
+            else PMap.add (fst h) (snd h) b  
         in
         List.fold_left pom PMap.empty lista
     in
     let graf = tworz_graf lista in 
-    let tworz_stan lista = (* utworzenie mapy liczb, która reprezentuje stany wierzchołków: 0 - nieprzetworzony; 1 - w trakcie przetwarzania; 2 - przetworzony *)
+    (** utworzenie mapy liczb, która reprezentuje stany wierzchołków:
+    0 <=> nieprzetworzony; 1 <=> w trakcie przetwarzania; 2 <=> przetworzony *)
+    let tworz_stan lista = 
         let pom a h =
-            let b = List.fold_left (fun a h -> PMap.add h 0 a) a (snd h) in (* mapa po przetworzeniu sąsiadów wierzchołka *) 
-            PMap.add (fst h) 0 b (* dołączenie wierzchołka do mapy *)
+            (** mapa po przetworzeniu sąsiadów wierzchołka *)
+            let b = List.fold_left (fun a h -> PMap.add h 0 a) a (snd h) in  
+            (** dołączenie wierzchołka do mapy *)
+            PMap.add (fst h) 0 b 
         in
         List.fold_left pom PMap.empty lista
     in
-    let rec dfs (stan, porzadek) wierzcholek = (* przeszukiwanie grafu w głąb *)
-        let sasiedzi = PMap.find wierzcholek graf in (* lista wierzchołków, do których jest krawędź z wierzchołka *)
-        let kolor = PMap.find wierzcholek stan in (* stan wierzchołka *)
+    (** przeszukiwanie grafu w głąb *)
+    let rec dfs (stan, porzadek) wierzcholek = 
+        (** lista wierzchołków, do których jest krawędź z wierzchołka *)
+        let sasiedzi = PMap.find wierzcholek graf in 
+        (** stan wierzchołka *)
+        let kolor = PMap.find wierzcholek stan in 
         match kolor with
-        | 0 -> (* wierzchołek jest odwiedzany po raz pierwszy *) 
+        | 0 -> 
             let (stan, porzadek) = List.fold_left (fun a h -> dfs a h) (PMap.add wierzcholek 1 stan, porzadek) sasiedzi in
-            (PMap.add wierzcholek 2 stan, wierzcholek::porzadek) 
-        | 1 -> raise Cykliczne (* wykryto cykl *)
-        | _ -> (stan, porzadek) (* wierzchołek już wcześniej został odwiedzony *)
+            (PMap.add wierzcholek 2 stan, wierzcholek :: porzadek) 
+        | 1 -> raise Cykliczne 
+        | _ -> (stan, porzadek)
     in
     let stan = tworz_stan lista in
+    (* przejście po wszystkich wierzchołkach początkowych z listy *)
     let (_, porzadek) = List.fold_left (fun a (h, _) -> if PMap.find h stan = 0 then dfs a h else a) (stan, []) lista in
     porzadek 
